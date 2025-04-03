bf723973c9e43105cafc4f5bfb88c418349966eb
sheismargret <margret.olushola@pau.edu.ng>
2025-04-03 11:40:37 +0100

    (my third commit)

---
+++ week_3/practice_2.py
@@ -0,0 +1,9 @@
+# Program to Check if a Number is Positive, negative or Zero
+
+num = float(input("Enter a number: "))
+if num > 0:
+    print("positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
